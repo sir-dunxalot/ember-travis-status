caac36f3781c39e1b614fda99952a0b960a92013
Duncan Graham Walker <walkerdu@bc.edu>
2015-06-20 20:59:48 -0400

    Unit test for helper promise

--- tests/unit/utils/get-travis-status-test.js
+++ tests/unit/utils/get-travis-status-test.js
@@ -1,10 +1,15 @@
-import getTravisStatus from '../../../utils/get-travis-status';
+import getTravisStatus from 'ember-travis-status/utils/get-travis-status';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | get travis status');
+module('Unit | Utility | get travis status', {
+  needs: ['service:travis-cache'],
+});
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var result = getTravisStatus();
-  assert.ok(result);
+  const result = getTravisStatus();
+
+  assert.ok(!!result.then,
+    'The helper should return a promise');
+
 });
